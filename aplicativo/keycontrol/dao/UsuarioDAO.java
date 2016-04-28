252a65eea35fc466cbed90a4a2e9057aec7c2906
ramonbrbs <ramonflmr@gmail.com>
2016-04-27 23:01:00 -0300

    Modificações UsuarioDAO
    Inserção

--- aplicativo/keycontrol/dao/UsuarioDAO.java
+++ aplicativo/keycontrol/dao/UsuarioDAO.java
@@ -12,7 +12,7 @@ import java.util.List;
 /*
  * @author Edu
  */
-public class UsuarioDAO implements GenericoDAO<Object>{
+public class UsuarioDAO implements GenericoDAO<UsuarioDTO>{
     
     public UsuarioDTO logar(UsuarioDTO usuario) throws PersistenciaException {
         UsuarioDTO usuarioR = null;
@@ -42,12 +42,28 @@ public class UsuarioDAO implements GenericoDAO<Object>{
     }
 
     @Override
-    public void inserir(Object obj) throws PersistenciaException {
+    public void inserir(UsuarioDTO usuario) throws PersistenciaException {
+        Connection con = ConexaoUtil.abrirConexao("Inserir Usuario");
+        String sql = "INSERT INTO usuario (id, nome, login, senha, tipo) values (NULL, ?,?,?,?)";
+        try{
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getLogin());
+            ps.setString(3, usuario.getSenha());
+            ps.setInt(1, usuario.getTipo());
+            ps.execute();
+            ps.close();
+        }catch (SQLException ex) {
+            throw new PersistenciaException(ex.getMessage(), ex);
+        } finally {
+            ConexaoUtil.fecharConexao(con);
+        }
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void atualizar(Integer id, Object obj) throws PersistenciaException {
+    public void atualizar(Integer id, UsuarioDTO obj) throws PersistenciaException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -57,12 +73,12 @@ public class UsuarioDAO implements GenericoDAO<Object>{
     }
 
     @Override
-    public List<Object> listarTodos() throws PersistenciaException {
+    public List<UsuarioDTO> listarTodos() throws PersistenciaException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Object buscarPorId(Integer id) throws PersistenciaException {
+    public UsuarioDTO buscarPorId(Integer id) throws PersistenciaException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
