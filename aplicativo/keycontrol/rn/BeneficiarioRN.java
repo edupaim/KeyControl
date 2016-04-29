a0e22ad1647f79afc134286a886117cf98a8ea8a
Toxad <filipinas_3@hotmail.com>
2016-04-29 17:07:26 -0300

    Implementação RN
    implementadas (eu espero) algumas funções da camada RN.

--- aplicativo/keycontrol/rn/BeneficiarioRN.java
+++ aplicativo/keycontrol/rn/BeneficiarioRN.java
@@ -6,5 +6,19 @@ import aplicativo.keycontrol.exception.NegocioException;
 import aplicativo.keycontrol.exception.PersistenciaException;
 
 public class BeneficiarioRN {
-	public BeneficiarioDTO validarMatricula(int matricula) throws NegocioException { /* ... */ }
+
+	private BeneficiarioRN(); // padrão singleton
+
+	public static BeneficiarioDTO validarMatricula(int matricula) throws NegocioException {
+		try {
+			if(matricula == 0 /* || matricula < 100000000 */) 
+				throw new NegocioException("Matrícula Inválida.");
+			BeneficiarioDTO beneficiario;
+			beneficiario = BeneficiarioDAO.validarMatricula(matricula);
+			return beneficiario;
+		}
+		catch (NegocioException | PersistenciaException ex) {
+			throw new NegocioException(ex.getMessage(), ex);
+		}
+	}
 }
--- aplicativo/keycontrol/rn/ChaveRN.java
+++ aplicativo/keycontrol/rn/ChaveRN.java
@@ -2,23 +2,68 @@ package aplicativo.keycontrol.rn;
 
 import aplicativo.keycontrol.dao.ChaveDAO;
 import aplicativo.keycontrol.dto.BeneficiarioDTO;
+import aplicativo.keycontrol.dto.ProfessorDTO;
 import aplicativo.keycontrol.dto.ChaveDTO;
 import aplicativo.keycontrol.exception.NegocioException;
 import aplicativo.keycontrol.exception.PersistenciaException;
 
 public class ChaveRN {
 
-	public boolean efetuarEmprestimo(BeneficiarioDTO benef, ChaveDTO chave) throws NegocioException { /* ... */ }
+	private ChaveRN();
+
+	public boolean efetuarEmprestimo(BeneficiarioDTO benef, ChaveDTO chave) throws NegocioException {
+		List<ChaveDTO> chaves;
+		try {
+			chaves = buscarChave(chave);
+			if(chaves.isEmpty())
+				throw new NegocioException("Chave não encontrada.");
+			if(chaves.size() > 1)
+				throw new NegocioException("Multiplas chaves encontradas.");
+			ChaveDTO chaveDAO = chaves.get(0);
+			if(chaveDAO.isAvailable(benef, chaveDAO)) 	// checar a instancof de benef;
+				return efetuarEmprestimo(benef, chave);
+			else
+				throw new NegocioException("Chave já emprestada ou reservada.");
+		}
+		catch (NegocioException | PersistenciaException ex) {
+			throw new NegocioException(ex.getMessage(), ex);
+		}
+	}
 	
-	public boolean efetuarReserva(int prof, ChaveDTO chave) throws NegocioException { /* ... */ }
+	public boolean efetuarReserva(int prof, ChaveDTO chave) throws NegocioException {
+		List<ChaveDTO> chaves;
+		try {
+			chaves = buscarChave(chave);
+			if(chaves.isEmpty())
+				throw new NegocioException("Chave não encontrada.");
+			if(chaves.size() > 1)
+				throw new NegocioException("Multiplas chaves encontradas.");
+			ChaveDTO chaveDAO = chaves.get(0);
+			if(chaveDAO.isReservable(benef, chaveDAO)) 	// checar a instancof de benef;
+				return efetuarReserva(benef, chave);
+			else
+				throw new NegocioException("Chave já emprestada ou reservada.");
+		}
+		catch (NegocioException | PersistenciaException ex) {
+			throw new NegocioException(ex.getMessage(), ex);
+		}
+	}
 	
-	public boolean cadastrarChave(ChaveDTO chave) throws NegocioException { /* ... */ }
+	public boolean cadastrarChave(ChaveDTO chave) throws NegocioException;
 
-	public List<ChaveDTO> listarChave() throws NegocioException { /* ... */ }
+	public List<ChaveDTO> listarChave() throws NegocioException {
+		try {
+			List<ChaveDTO> chaves = ChaveDAO.listarTodos();
+			return chaves;
+		}
+		catch (PersistenciaException ex) {
+			throw new NegocioException(ex.getMessage(), ex);
+		}
+	}
 
-	public boolean buscarChave(ChaveDTO chave) throws NegocioException { /* ... */ }
+	public List<ChaveDTO> buscarChave(ChaveDTO chave) throws NegocioException;
 
-	public boolean devolucaoChave(BeneficiarioDTO benef, ChaveDTO chave) throws NegocioException { /* ... */ }
+	public boolean devolucaoChave(BeneficiarioDTO benef, ChaveDTO chave) throws NegocioException;
 
-	public boolean alterarChave(ChaveDTO chave) throws NegocioException { /* ... */ }
+	public boolean alterarChave(ChaveDTO chave) throws NegocioException;
 }
