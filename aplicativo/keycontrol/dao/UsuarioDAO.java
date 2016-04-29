69f68f2cea15501bef7f41ec9c1ba7982af70c5b
ramonbrbs <ramonflmr@gmail.com>
2016-04-29 00:09:44 -0300

    Implementação DAO
    Usuario DAO completo

--- aplicativo/keycontrol/dao/UsuarioDAO.java
+++ aplicativo/keycontrol/dao/UsuarioDAO.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -42,15 +43,15 @@ public class UsuarioDAO implements GenericoDAO<UsuarioDTO>{
     }
 
     @Override
-    public void inserir(UsuarioDTO usuario) throws PersistenciaException {
+    public void inserir(UsuarioDTO u) throws PersistenciaException {
         Connection con = ConexaoUtil.abrirConexao("Inserir Usuario");
         String sql = "INSERT INTO usuario (id, nome, login, senha, tipo) values (NULL, ?,?,?,?)";
         try{
             PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1, usuario.getNome());
-            ps.setString(2, usuario.getLogin());
-            ps.setString(3, usuario.getSenha());
-            ps.setInt(1, usuario.getTipo());
+            ps.setString(1, u.getNome());
+            ps.setString(2, u.getLogin());
+            ps.setString(3, u.getSenha());
+            ps.setInt(4, u.getTipo());
             ps.execute();
             ps.close();
         }catch (SQLException ex) {
@@ -63,23 +64,97 @@ public class UsuarioDAO implements GenericoDAO<UsuarioDTO>{
     }
 
     @Override
-    public void atualizar(Integer id, UsuarioDTO obj) throws PersistenciaException {
+    public void atualizar(Integer id, UsuarioDTO u) throws PersistenciaException {
+        Connection con = ConexaoUtil.abrirConexao("Inserir Usuario");
+        String sql = "UPDATE usuario set nome = ?, login = ?, senha = ?, tipo = ? WHERE id = ?";
+        try{
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, u.getNome());
+            ps.setString(2, u.getLogin());
+            ps.setString(3, u.getSenha());
+            ps.setInt(4, u.getTipo());
+            ps.setInt(5, u.getTipo());
+            ps.execute();
+            ps.close();
+        }catch (SQLException ex) {
+            throw new PersistenciaException(ex.getMessage(), ex);
+        } finally {
+            ConexaoUtil.fecharConexao(con);
+        }
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void deletar(Integer id) throws PersistenciaException {
+        Connection con = ConexaoUtil.abrirConexao("Inserir Usuario");
+        String sql = "DELETE FROM usuario WHERE id = ?";
+        try{
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
+            ps.close();
+        }catch (SQLException ex) {
+            throw new PersistenciaException(ex.getMessage(), ex);
+        } finally {
+            ConexaoUtil.fecharConexao(con);
+        }
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public List<UsuarioDTO> listarTodos() throws PersistenciaException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<UsuarioDTO> retorno = new ArrayList<UsuarioDTO>();
+        Connection con = ConexaoUtil.abrirConexao("Login");
+        String sql = "select * from usuario ";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                UsuarioDTO u = new UsuarioDTO();
+                u.setId(rs.getInt(1));
+                u.setLogin(rs.getString(3));
+                u.setNome(rs.getString(2));
+                u.setSenha(rs.getString(4));
+                u.setTipo(rs.getInt(5));
+                retorno.add(u);
+            }
+            
+        } catch (SQLException ex) {
+            throw new PersistenciaException(ex.getMessage(), ex);
+        } finally {
+            ConexaoUtil.fecharConexao(con);
+        }
+        return retorno;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public UsuarioDTO buscarPorId(Integer id) throws PersistenciaException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        UsuarioDTO usuarioR = null;
+        Connection con = ConexaoUtil.abrirConexao("Login");
+        String sql = "select * from usuario ";
+        sql += "where id = ? ";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                usuarioR = new UsuarioDTO();
+                usuarioR.setId(rs.getInt(1));
+                usuarioR.setLogin(rs.getString(3));
+                usuarioR.setNome(rs.getString(2));
+                usuarioR.setSenha(rs.getString(4));
+                usuarioR.setTipo(rs.getInt(5));
+            }
+        } catch (SQLException ex) {
+            throw new PersistenciaException(ex.getMessage(), ex);
+        } finally {
+            ConexaoUtil.fecharConexao(con);
+        }
+        return usuarioR;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
