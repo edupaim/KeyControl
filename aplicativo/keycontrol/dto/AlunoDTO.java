1c6315a51ffb131b635f2ffb0c81d34644dd5d2b
ramonbrbs <ramonflmr@gmail.com>
2016-05-12 17:46:08 -0300

    DTO Aluno / Professor
    Criação dos dtos referentes ao beneficiario p/ emprestimo

---
+++ aplicativo/keycontrol/dto/AlunoDTO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicativo.keycontrol.dto;
+
+/**
+ *
+ * @author ramon
+ */
+public class AlunoDTO implements IBeneficiarioDTO{
+    private int id;
+    private String nome;
+    private String matricula;
+    
+    public int getId(){
+        return this.id;
+    }
+
+ 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
---
+++ aplicativo/keycontrol/dto/IBeneficiarioDTO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicativo.keycontrol.dto;
+
+/**
+ *
+ * @author ramon
+ */
+public interface IBeneficiarioDTO {
+    public int getId();
+    public void setId(int id);
+    public String getNome();
+    public void setNome(String nome);
+    public String getMatricula();
+    public void setMatricula(String nome);
+    
+    
+}
